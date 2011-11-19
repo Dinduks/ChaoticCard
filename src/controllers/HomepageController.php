f4e7701da386e75e4107cee3e2712fc4b547b7b5
Samy Dindane <samy@dindane.com>
2011-11-19 19:20:34 +0100

    Added email addresses protection from spam crawlers

--- src/controllers/HomepageController.php
+++ src/controllers/HomepageController.php
@@ -21,13 +21,24 @@ class HomepageController
         $websites = WebsiteTable::getAllWebsites($db);
         $phonenumbers = PhoneNumberTable::getAllPhoneNumbers($db);
         
+        // transforms the email adresses to a string of ASCII chars
+        // (to protect them from spam crawlers)
+        $emailsArray = array();
+        foreach ($emails as $i=>$email) {
+            $strEmail = str_split($email->getEmail());
+            $emailsArray[$i] = '';
+            foreach ($strEmail as $char) {
+                $emailsArray[$i] .= '&#' . ord($char) . ';';
+            }
+        }
+        
         $about = TextTable::getText($db, $this->app['lang'], 'about')->getText();
         $secondaryTitle = TextTable::getText($db, $this->app['lang'], 'secondaryTitle')->getText();
 
         return $this->app['twig']->render('homepage.html.twig', array(
                     'card' => $card,
                     'links' => $links,
-                    'emails' => $emails,
+                    'emails' => $emailsArray,
                     'websites' => $websites,
                     'phonenumbers' => $phonenumbers,
                     'about' => $about,
