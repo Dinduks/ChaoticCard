24506e6d44a6c3073155626ddd728e48c0e8e54a
Samy Dindane <samy@dindane.com>
2011-11-26 18:44:48 +0100

    getClientLanguage() has now a defaultLanguage param

--- vendor/ChaoticCardUtil.php
+++ vendor/ChaoticCardUtil.php
@@ -3,13 +3,13 @@
 class ChaoticCardUtil 
 {
 
-    public static function getClientLanguage() 
+    public static function getClientLanguage($defaultLang = 'en') 
     {
         if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
             $langs = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
             return substr($langs[0], 0, 2);
         } else {
-            return 'en';
+            return $defaultLang;
         }
     }
 
