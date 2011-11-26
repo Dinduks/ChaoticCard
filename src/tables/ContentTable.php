186578f25fb375cc40ff6433fd34fff865abad48
Samy Dindane <samy@dindane.com>
2011-11-26 17:19:16 +0100

    Corrected a mistake in ContentTable::save()

--- src/tables/ContentTable.php
+++ src/tables/ContentTable.php
@@ -6,7 +6,7 @@ class ContentTable
     public static function save(\Doctrine\DBAL\Connection $db, 
             Models\Content $content)
     {
-        $query = "INSERT INTO content(langId, textId)
+        $query = "INSERT INTO content(lang_id, text_id)
                   VALUES
                   ('" . $content->getLangId() . "', '" . $content->getTextId() . "')";
         $result = $db->executeQuery($query);
